b5c9b50649363649bd11cefe2bf08654d0183326
Mark Dewey <mark.t.dewey@gmail.com>
2018-09-10 16:28:42 -0400

    updated path for this cohort

--- Koans/PathToEnlightenment.cs
+++ Koans/PathToEnlightenment.cs
@@ -8,25 +8,21 @@ namespace DotNetCoreKoans.Koans
         public PathToEnlightenment()
         {
             Types = new Type[] {
-				typeof(AboutAsserts),
-				typeof(AboutNull),
-				typeof(AboutArrays),
-				typeof(AboutArrayAssignment),
-				typeof(AboutStrings),
+                typeof(AboutAsserts),
+                typeof(AboutNull),
+                typeof(AboutArrays),
+                typeof(AboutArrayAssignment),
+                typeof(AboutStrings),
                 typeof(AboutFloats),
-				typeof(AboutInheritance),
+                typeof(AboutInheritance),
                 typeof(AboutMethods),
                 typeof(AboutControlStatements),
                 typeof(AboutGenericContainers),
                 typeof(AboutLambdas),
                 typeof(AboutLinq),
-        typeof(AboutBitwiseAndShiftOperator)
-                //TODO: disabled due to missing functionality in netcoreapp1.0
-                // it will be available in 1.1 see:
-                // https://github.com/dotnet/coreclr/blob/master/src/mscorlib/src/System/Array.cs#L1005
-                //typeof(AboutDelegates),
-                //typeof(AboutLambdas)
+                typeof(AboutDelegates),
+                typeof(AboutBitwiseAndShiftOperator),
                 };
-        } 
+        }
     }
 }
